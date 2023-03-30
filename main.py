a2beef2dca72874ac5ad793293c1415c9775d3ba
kenan-erol <kenan.erol@yale.edu>
2023-03-30 14:08:40 -0400

    Initialize files from 3/30

---
+++ main.py
@@ -0,0 +1,5 @@
+# # 3/30 coding notes
+# dynamic web server vs static web server
+# static returns same response, dynamic gives diff response to same request
+# flask is dynamic 
+
---
+++ sketch.js
@@ -0,0 +1,8 @@
+// phaser p5js.org
+function setup(){
+    null;
+}
+
+function draw(){
+    null;
+}
